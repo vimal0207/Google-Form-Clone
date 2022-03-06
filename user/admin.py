502239fea5847700ee0f3603c554246c203bc0d9
vimal <vimal.mahawar@vodenicely.in>
2022-03-06 17:07:40 +0530

    gitignore

--- user/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from user import models as user_models
-
-@admin.register(user_models.User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'email', 'created','modified')
-    search_fields = ["id", "email"]
--- user/serializer.py
+++
@@ -1,3 +0,0 @@
-from rest_framework import serializers
-from user import models as user_models
-from master_file import keys
